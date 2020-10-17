448cb2960f158b36499f39c9b83e343e3af63c73
aki-it <yoshiaki.sato.20@gmail.com>
2020-10-18 03:49:18 +0900

    add Score Sheet

--- CATAN/CATAN/ContentView.swift
+++ CATAN/CATAN/ContentView.swift
@@ -13,8 +13,21 @@ struct ContentView: View {
     let plus = " + "
     let equal = " = "
     @State private var number = " "
+    @State private var showSheet = false
     var body: some View {
         VStack {
+            if number == "7"{
+                Text(number)
+                    .font(.system(size: 200))
+                    .fontWeight(.bold)
+                    .foregroundColor(.red)
+            } else {
+                Text(number)
+                    .font(.system(size: 200))
+                    .fontWeight(.bold)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            }
+            
             Button(action: {
                     self.number = String((Int(self.dice1.randomElement()!)! + Int(self.dice2.randomElement()!)!))
                 }) {
@@ -22,27 +35,38 @@ struct ContentView: View {
                         .font(.largeTitle)
                         .padding()
                         .foregroundColor(.black)
-                        .background(Capsule().foregroundColor(.gray))
-                    }
-        
-            if number == "7"{
-                Text(number)
-                    .font(.system(size: 150))
-                    .fontWeight(.bold)
-                    .foregroundColor(.red)
-            } else {
-                Text(number)
-                    .font(.system(size: 150))
-                    .fontWeight(.bold)
-                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        .background(Capsule().foregroundColor(.white))
             }
-
+            Text(" ")
+            
+            Button(action: {
+                self.showSheet.toggle()
+            }) {
+               Text("Score Sheet")
+                .font(.largeTitle)
+            }
+            .sheet(isPresented: $showSheet){
+                MySheet()
+            }
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            MySheet()
+        }
+    }
+}
+
+struct MySheet: View{
+    var body: some View {
+        ZStack {
+            Color.white
+            Text("Score")
+                .font(.largeTitle)
+        }
     }
 }
